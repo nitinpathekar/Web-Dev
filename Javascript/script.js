3099773b591086821ac7990f601b1bb61e7ec750
User <nitinsardarpathekar@gmail.com>
2024-07-14 11:53:01 +0530

    updat the code

---
+++ Javascript/script.js
@@ -0,0 +1,25 @@
+// console.log("nitin");
+// var a=12;
+// function abcs(){
+
+//     var a=11;
+//     function def(){
+//         var b=12;
+
+//     }
+// }
+// var a=[12,334,4,2,432,1234,4,3];
+// var b=[...a];
+
+// a.forEach(function(val) {
+//     console.log(val+1);
+    
+// });
+var obj ={
+    name :"nitin",
+    age: 18,
+    city  :"betul"
+}
+for (var key in obj){
+    onj[key]
+}
