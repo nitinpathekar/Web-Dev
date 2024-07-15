7d61bba5ac49b806e1d1ac0f783188af0bcb41d8
User <nitinsardarpathekar@gmail.com>
2024-07-15 21:13:08 +0530

    array

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,66 +1,83 @@
-// // // // // // var name_="Nitin";
-// // // // // // //var name_="Pawar"(not supported)
+// // // // // // // // // var name_="Nitin";
+// // // // // // // // // //var name_="Pawar"(not supported)
 
-// // // // // // let name ="Nitin Pathekar";
-// // // // // // let age = 20;
-// // // // // // let totalPrice =1000;
-// // // // // // // let name ="nitin";(not supported)
-// // // // // // // name ="nitin";(supported)
+// // // // // // // // // let name ="Nitin Pathekar";
+// // // // // // // // // let age = 20;
+// // // // // // // // // let totalPrice =1000;
+// // // // // // // // // // let name ="nitin";(not supported)
+// // // // // // // // // // name ="nitin";(supported)
 
-// // // // // // const pi=3.14;
-// // // // // // // const pi=3;(not supported)
-// // // // // // // pi=3;(not supported)
+// // // // // // // // // const pi=3.14;
+// // // // // // // // // // const pi=3;(not supported)
+// // // // // // // // // // pi=3;(not supported)
 
-// // // // // // const
-// // // // // // // const a;
-// // // // // function printname(n){
-// // // // // for (let i = 0;i<n;i++){
-// // // // //     console.log("Nitin Pathekar");
-// // // // // }
-// // // // // }
-// // // // // printname(4 );
-
-
-
-// // // // function sum(start, end){
-// // // //     let sum=0;
-// // // //     for (let i=start;i<=end;i++){
-// // // //        sum+=i;
-// // // //     }
-// // // //     return sum;
-// // // // }
-// // // // console.log(sum(8,9));
-// // // let i=1,sum=0;
-// // // let end=prompt("till");
-// // // while(i<end)
-// // //     {
-// // //         sum+=i;
-// // //         i++
-// // //     };
-
-
-// // let obj ={
-// //     item :"pen",
-// //     price:10,
-// // };
-// // let output=`the cost of  ${obj.item} is ${obj.price}`;
-// // console.log(output);
-// // console.log("the cost of",obj.item,"is",obj.price);
-// // let specialString=`this is a temp literal`;
-// // console.log(typeof specialString);
-
-// console.log("Nitin\nPathekar")
-// // console.log("Nitin\tPathekar")
-
-// string methodin js
+// // // // // // // // // const
+// // // // // // // // // // const a;
+// // // // // // // // function printname(n){
+// // // // // // // // for (let i = 0;i<n;i++){
+// // // // // // // //     console.log("Nitin Pathekar");
+// // // // // // // // }
+// // // // // // // // }
+// // // // // // // // printname(4 );
 
 
 
-let s=("string")
-console.log(s.toLocaleUpperCase());
-console.log(s.slice(1,3))
-console.log(s)
-let s2="nitin"
-console.log(s.concat(s2))
-console.log(s.replace("r","l"))
-console.log(s.charAt(5))
+// // // // // // // function sum(start, end){
+// // // // // // //     let sum=0;
+// // // // // // //     for (let i=start;i<=end;i++){
+// // // // // // //        sum+=i;
+// // // // // // //     }
+// // // // // // //     return sum;
+// // // // // // // }
+// // // // // // // console.log(sum(8,9));
+// // // // // // let i=1,sum=0;
+// // // // // // let end=prompt("till");
+// // // // // // while(i<end)
+// // // // // //     {
+// // // // // //         sum+=i;
+// // // // // //         i++
+// // // // // //     };
+
+
+// // // // // let obj ={
+// // // // //     item :"pen",
+// // // // //     price:10,
+// // // // // };
+// // // // // let output=`the cost of  ${obj.item} is ${obj.price}`;
+// // // // // console.log(output);
+// // // // // console.log("the cost of",obj.item,"is",obj.price);
+// // // // // let specialString=`this is a temp literal`;
+// // // // // console.log(typeof specialString);
+
+// // // // console.log("Nitin\nPathekar")
+// // // // // console.log("Nitin\tPathekar")
+
+// // // // string methodin js
+
+
+
+// // // let s=("string")
+// // // console.log(s.toLocaleUpperCase());
+// // // console.log(s.slice(1,3))
+// // // console.log(s)
+// // // let s2="nitin"
+// // // console.log(s.concat(s2))
+// // // console.log(s.replace("r","l"))
+// // // console.log(s.charAt(5))
+
+
+// // let name=prompt("Enter your name :");
+// // let username=`@${name}${name.length}`;
+// // console.log(username);
+
+// let array=[85,97,44,37,76,60];
+// function avg(array){
+//     let sum=0;
+//     for(let i=0;i<array.length;i++){
+//         sum+=array[i];
+//     }
+//    return sum/array.length;
+// }
+// console.log(
+//     avg(array)
+// )
